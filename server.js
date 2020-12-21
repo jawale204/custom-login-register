046297755c88949f518d8a2f6c6de6651e8d1e7d
jawale204 <jawale204@gmail.com>
2020-12-21 16:42:57 +0530

    1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express()
 const ConnectDB=require('./config/db')
-const PORT=3000||process.env.PORT
+const PORT=process.env.PORT||3000
 ConnectDB()
 
 app.use(express.json());
